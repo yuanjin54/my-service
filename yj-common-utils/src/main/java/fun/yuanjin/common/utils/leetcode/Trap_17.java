7af70e4afeff5e5bb9d98c4b5029336d95f3bab9
yuanjin <yuanjin03@meituan.com>
2021-04-02 10:46:15 +0800

    面试题 17.21. 直方图的水量

---
+++ yj-common-utils/src/main/java/fun/yuanjin/common/utils/leetcode/Trap_17.java
@@ -0,0 +1,34 @@
+package fun.yuanjin.common.utils.leetcode;
+
+/**
+ * @ClassName Trap_17
+ * @Description 面试题 17.21. 直方图的水量
+ * 给定一个直方图(也称柱状图)，假设有人从上面源源不断地倒水，最后直方图能存多少水量?直方图的宽度为 1。
+ * https://leetcode-cn.com/problems/volume-of-histogram-lcci/
+ * @Author yuanjin
+ * @Date 2021-04-02 10:14
+ * @Version 1.0
+ */
+public class Trap_17 {
+    public static void main(String[] args) {
+        System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+    }
+
+    public static int trap(int[] height) {
+        int ans = 0;
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        while (left < right) {
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if (height[left] < height[right]) {
+                ans += leftMax - height[left];
+                ++left;
+            } else {
+                ans += rightMax - height[right];
+                --right;
+            }
+        }
+        return ans;
+    }
+}
